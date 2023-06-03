7f91c8656b82390b8a679fcde2f5f11a4126e0e3
Cody Bergin <cberg79@wgu.edu>
2023-06-02 19:06:35 -0600

    thirteenth save

--- src/sample/Controller/LoginController.java
+++ src/sample/Controller/LoginController.java
@@ -56,6 +56,8 @@ public class LoginController implements Initializable {
     @FXML
     void onActionLogin(ActionEvent event) throws IOException {
 
+        String userName = usernameTxt.getText()
+
         stage = (Stage)((Button)event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("/sample/View/MainMenu.fxml"));
         stage.setScene(new Scene(scene));
@@ -68,13 +70,14 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         zoneIdLabel.setText(ZoneId.systemDefault().toString());
-        resourceBundle = ResourceBundle.getBundle("sample.Language", new Locale("fr"));
-      //  resourceBundle = ResourceBundle.getBundle("Language", Locale.getDefault());
+      //  resourceBundle = ResourceBundle.getBundle("sample.Language", new Locale("fr"));
+        resourceBundle = ResourceBundle.getBundle("sample.Language", Locale.getDefault());
         title.setText(resourceBundle.getString("title"));
         username.setText(resourceBundle.getString("username"));
         password.setText(resourceBundle.getString("password"));
         loginBtn.setText(resourceBundle.getString("loginBtn"));
         cancelBtn.setText(resourceBundle.getString("cancelBtn"));
+        location.setText(resourceBundle.getString("location"));
 
     }
 }
--- src/sample/DAO/DBUser.java
+++ src/sample/DAO/DBUser.java
@@ -1,4 +1,24 @@
 package sample.DAO;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBUser {
+
+    public static boolean validateLogin(String username, String password) throws SQLException {
+
+        String sql = "SELECT * FROM users WHERE User_Name = '" + username + "' AND Password = '" + password + "'";
+        PreparedStatement ps = JDBC.connection.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        if (rs.getString("User_Name").equals(username) && rs.getString("Password").equals(password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
 }
